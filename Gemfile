ce7e4a9925c122f1a2c4f0cca2681fc6006f3d12
Tatiana Medeiros <tatiana@chdigital.com.br>
2018-01-02 14:50:21 -0200

    add tests model user

--- Gemfile
+++ Gemfile
@@ -29,12 +29,12 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.5'
+  gem 'factory_bot_rails'
 end
 
 group :test do
   gem 'database_cleaner'
   gem 'shoulda-matchers', '~> 3.1'
-  gem 'factory_girl_rails'
   gem 'faker'
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryBot.build(:user) }
+  it { expect(@user).to respond_to(:email) }
+  it { expect(@user).to respond_to(:name) }
+  it { expect(@user).to respond_to(:password) }
+  it { expect(@user).to respond_to(:password_confirmation) }
+  it { expect(@user).to be_valid}
+
+
+  # pending "add some examples to (or delete) #{__FILE__}"
+
 end
