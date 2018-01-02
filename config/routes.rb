2d2926a6b8c9a0dbae5c03cc2d418b071987cf0d
Tatiana Medeiros <tatiana@chdigital.com.br>
2018-01-02 13:06:37 -0200

    add version api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
+require 'api_version_contraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
-  end
+    namespace :v1, path: '/', constraints: ApiVersionContraint.new(version: 1, default: true) do
+      
+    end
 
+    # namespace :v2, path: '/', constraints: ApiVersionContraint.new(version: 2, default: true) do
+    #   resources :tasks
+    # end
+
+  end
+  
 end
