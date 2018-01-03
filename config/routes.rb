be677bd5ecd1e9b848d538ebefce5d9d959b9687
Tatiana Medeiros <tatiana@chdigital.com.br>
2018-01-03 10:48:08 -0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
     namespace :v1, path: '/', constraints: ApiVersionContraint.new(version: 1, default: true) do
-      
+      resources :users, only: [:show]
     end
 
     # namespace :v2, path: '/', constraints: ApiVersionContraint.new(version: 2, default: true) do
